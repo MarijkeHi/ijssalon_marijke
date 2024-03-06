d10a5f4aeaf6b089eec19a794fa4b3f448b8a25c
Marijke Hillebrand <hilliebillie73@hotmail.com>
2024-03-06 10:28:46 +0100

    functies toevoegen

--- tijdelijk.py
+++ tijdelijk.py
@@ -1,16 +1,22 @@
-prijzen={
-    "aardbei":3,
-    "vanille":4,
-    "chocolade":5
-}
-aanbieding=(prijzen["aardbei"] * 0.8)
-reclame_tekst=(f"Vandaag in de aanbieding: aardbeien-ijs, 1 liter - slechts E {aanbieding}")
-reclame_tekst2=reclame_tekst[:65]
-reclame_tekst3=reclame_tekst2.upper()
-reclame_tekst4=reclame_tekst3.split(" ")
-el=reclame_tekst2.lower()
-for item in reclame_tekst4:
-    if len(item)>=5:
-        print (item.upper())
-    else:
-        print (item.lower())
+from helper import decoreer
+
+def print_aanbieding():
+    prijzen={
+        "aardbei":3,
+        "vanille":4,
+        "chocolade":5
+    }
+    aanbieding=prijzen["aardbei"] * 0.8
+    reclame_tekst=(f"Vandaag in de aanbieding: aardbeien-ijs, 1 liter - slechts E {aanbieding}")
+    reclame_tekst2=reclame_tekst[:65]
+    reclame_tekst3=reclame_tekst2.upper()
+    reclame_tekst4=reclame_tekst3.split(" ")
+    el=reclame_tekst2.lower()
+    for item in reclame_tekst4:
+        if len(item)>=5:
+            print (item.upper())
+        else:
+            print (item.lower())
+
+decoreer("Aanbieidng")
+print_aanbieding()
